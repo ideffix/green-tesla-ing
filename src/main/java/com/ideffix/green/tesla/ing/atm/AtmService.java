2fe23fa49620a8d5b0ab158310d10ff39cf5b01b
IdeFFiX <ideffix1337@gmail.com>
2023-05-05 14:42:23 +0200

    simplify AtmService

--- src/main/java/com/ideffix/green/tesla/ing/atm/AtmService.java
+++ src/main/java/com/ideffix/green/tesla/ing/atm/AtmService.java
@@ -7,46 +7,16 @@ public class AtmService {
     public List<ATM> calculateOrder(List<Task> tasks) {
         tasks.sort(new TaskComparator());
 
+        Set<ATM> seen = new HashSet<>();
         List<ATM> result = new ArrayList<>();
 
-        List<ATM> failureRestartAtms = new ArrayList<>();
-        List<ATM> priorityAtms = new ArrayList<>();
-        List<ATM> signalLowAtms = new ArrayList<>();
-        List<ATM> standardAtms = new ArrayList<>();
-
-        Set<ATM> alreadyUsedAtm = new HashSet<>();
-
-        int lastRegion = -1;
         for (Task task : tasks) {
-            if (task.region() != lastRegion) {
-                result.addAll(failureRestartAtms);
-                result.addAll(priorityAtms);
-                result.addAll(signalLowAtms);
-                result.addAll(standardAtms);
-
-                failureRestartAtms = new ArrayList<>();
-                priorityAtms = new ArrayList<>();
-                signalLowAtms = new ArrayList<>();
-                standardAtms = new ArrayList<>();
-            }
-
             ATM atm = new ATM(task.region(), task.atmId());
-            if (alreadyUsedAtm.contains(atm)) {
-                continue;
-            }
-            alreadyUsedAtm.add(atm);
-
-            switch (task.requestType()) {
-                case STANDARD -> standardAtms.add(atm);
-                case PRIORITY -> priorityAtms.add(atm);
-                case SIGNAL_LOW -> signalLowAtms.add(atm);
-                case FAILURE_RESTART -> failureRestartAtms.add(atm);
+            if (!seen.contains(atm)) {
+                seen.add(atm);
+                result.add(atm);
             }
         }
-        result.addAll(failureRestartAtms);
-        result.addAll(priorityAtms);
-        result.addAll(signalLowAtms);
-        result.addAll(standardAtms);
 
         return result;
     }
